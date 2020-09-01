014d83c96364e2ed54a1a385ea852ecd5058e48a
夏末知秋 <970217411@qq.com>
2020-09-01 15:14:31 +0800

    提交

--- ssos-web/src/test/java/com/ssos/study/JDKProxyTest.java
+++ ssos-web/src/test/java/com/ssos/study/JDKProxyTest.java
@@ -1,5 +1,6 @@
 package com.ssos.study;
 
+
 import sun.misc.ProxyGenerator;
 
 import java.lang.reflect.Constructor;
@@ -13,34 +14,34 @@ import java.nio.file.Paths;
 
 public class JDKProxyTest {
     public static void main(String[] args) throws Exception {
-        Student student = new StudentImpl();
-
-        Student studentProxy = (Student) Proxy.newProxyInstance(student.getClass().getClassLoader(),
-                new Class[]{Student.class},
-                (Object proxy, Method method, Object[] argss) -> {
-                    System.out.println("我是内存中的前置");
-                    method.invoke(student);
-                    return proxy;
-                });
-        studentProxy.getStudent();
-
-        String studentProxyClassName = studentProxy.getClass().toString();
-        studentProxyClassName = studentProxyClassName.substring(studentProxyClassName.lastIndexOf(".") + 2);
-        byte[] bytes = ProxyGenerator.generateProxyClass(studentProxyClassName,new Class[]{Student.class});
-        Files.write(Paths.get("/Volumes/Mac/www/"+studentProxyClassName+".class"),bytes);
-
-
-        URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("file:/Volumes/Mac/www/")});
-        Class<?> proxy = classLoader.loadClass("Proxy0");
-        Constructor<?> declaredConstructors = proxy.getConstructor(InvocationHandler.class);
-        Student student1 = (Student) declaredConstructors.newInstance(new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] argss) throws Throwable {
-                System.out.println("我是加载class文件的前置");
-                method.invoke(student, argss);
-                return proxy;
-            }
-        });
-        student1.getStudent();
+//        Student student = new StudentImpl();
+//
+//        Student studentProxy = (Student) Proxy.newProxyInstance(student.getClass().getClassLoader(),
+//                new Class[]{Student.class},
+//                (Object proxy, Method method, Object[] argss) -> {
+//                    System.out.println("我是内存中的前置");
+//                    method.invoke(student);
+//                    return proxy;
+//                });
+//        studentProxy.getStudent();
+//
+//        String studentProxyClassName = studentProxy.getClass().toString();
+//        studentProxyClassName = studentProxyClassName.substring(studentProxyClassName.lastIndexOf(".") + 2);
+//        byte[] bytes = ProxyGenerator.generateProxyClass(studentProxyClassName,new Class[]{Student.class});
+//        Files.write(Paths.get("/Volumes/Mac/www/"+studentProxyClassName+".class"),bytes);
+//
+//
+//        URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("file:/Volumes/Mac/www/")});
+//        Class<?> proxy = classLoader.loadClass("Proxy0");
+//        Constructor<?> declaredConstructors = proxy.getConstructor(InvocationHandler.class);
+//        Student student1 = (Student) declaredConstructors.newInstance(new InvocationHandler() {
+//            @Override
+//            public Object invoke(Object proxy, Method method, Object[] argss) throws Throwable {
+//                System.out.println("我是加载class文件的前置");
+//                method.invoke(student, argss);
+//                return proxy;
+//            }
+//        });
+//        student1.getStudent();
     }
 }
--- ssos-web/src/test/java/com/ssos/study/StudentImpl.java
+++ ssos-web/src/test/java/com/ssos/study/StudentImpl.java
@@ -1,9 +1,27 @@
 package com.ssos.study;
 
-public class StudentImpl implements Student {
-    @Override
-    public void getStudent() {
-        System.out.println("学习");
+import com.ssos.SsosApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SsosApplication.class)
+@Slf4j
+
+public class StudentImpl {
+
+    @Test
+    public void test() {
+        RestHighLevelClient restHighLevelClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        System.out.println(restHighLevelClient);
     }
 
 }
