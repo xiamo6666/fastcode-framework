fa1f2011b47e7a67611c36f681be8fa8147641b5
夏末知秋 <970217411@.com>
2022-04-20 21:32:36 +0800

    项目版本大升级

---
+++ fc-core/src/test/java/com/fc/core/MainTest.java
@@ -0,0 +1,9 @@
+package com.fc.core;
+
+import java.util.Map;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println(Map.of("我草","我草"));
+    }
+}
---
+++ fc-system/src/main/java/com/fc/system/SystemApplication.java
@@ -0,0 +1,18 @@
+package com.fc.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @className : SystemApplication
+ * @description : dto
+ * @author : xwl
+ * @date : 2022/4/20 21:26
+ * @version : 1.0
+ */
+@SpringBootApplication
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
+}
